0c97d4c9980cddd274c11f18e1b06e7f6f8accc1
Dustin <leeddustin@gmail.com>
2024-04-19 23:04:21 -0700

    created matrix class which will hold all our types of function calls

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class MatrixClass{
+public:
+    void matrix(int& rows, int& columns){
+        
+    }
+
+    /*int rref(){}
+
+    int wronskian(){}
+
+    int cramers(){}*/
+};
+
+    int main(){
+        int rows, columns;
+        cout << "How many rows and colums?: ";
+        cin >> rows >> columns;
+        MatrixClass matrix1;
+        matrix1.matrix(rows, columns);
+
+    }
+
