de5bcea50fa26dc23d839172ef99160d425d93c9
Dustin <leeddustin@gmail.com>
2024-04-20 13:32:12 -0700

    changed array to vecotrs.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,33 @@ using namespace std;
 
 class Matrix{
 public:
-    template <int rows, int columns>
-    void input(int (&matrixArray)[rows][columns]){
-        
+    void display(const vector<vector<int>>& matrix) {
+        int i, j;
+        cout<<"\n";
+        for(const auto& row : matrix) {
+            for(int val : row) {
+                cout<<" ";
+                cout<<val;
+            }
+            cout<<"\n";
+        }
     }
 
-    /*int rref(){}
-    int wronskian(){}
+    int wronskian(const vector<vector<int>>& matrix){
+        int rows = matrix.size();
+        int columns = matrix[0].size();
+        if(rows != columns){
+            cout << "Can not find wronskian of an unbalanced matrix.\n";
+            return 0;
+        }
+        if(rows == 2)
+           return (matrix[0][0] * matrix[0][1]) - (matrix[1][0] * matrix[1][1]);
+        
+        return 0;
+    }
+    
+    /*  
+    int rref(){}
     int cramers(){}*/
 };
 
@@ -19,14 +39,17 @@ public:
         cout << "How many rows and colums?: ";
         cin >> rows >> columns;
 
-        int matrixArray[rows][columns];
+        vector<vector<int>> matrixArr(rows, vector<int>(columns));
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < columns; j++){
                 cout << "["<<i<<"]["<<j<<"]: ";
-                cin >> matrixArray[i][j];
+                cin >> matrixArr[i][j];
             }
         }
-        
 
+        Matrix m1;
+        m1.display(matrixArr);
+
+        return 0;
     }
 
