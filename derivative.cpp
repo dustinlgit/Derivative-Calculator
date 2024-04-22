0e3210221e422e83a4b73deacf5a96e55a77b626
Dustin <leeddustin@gmail.com>
2024-04-22 12:41:52 -0700

    created stack data struct

---
+++ derivative.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "stack.h"
+
+
+using namespace std;
+
+int derivative(string value){
+    
+}
+
+int main(){
+    Stack stack1;
+    
+    string derivative;
+    cout << "What is your derivative?: \n";
+
+    cin >> derivative;
+}
